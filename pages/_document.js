e929c3e07253186dba53b6d1287e15f97d8c0aa4
James McArthur <77163783+jamezmca@users.noreply.github.com>
2022-02-28 09:48:28 -0800

    adding cdn for font awesome icons

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+                    integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA=="
+                    crossorigin="anonymous" referrerPolicy="no-referrer" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
