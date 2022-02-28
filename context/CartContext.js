99faa25a705a2b20a000571c39960e4671386c53
James McArthur <77163783+jamezmca@users.noreply.github.com>
2022-02-28 11:17:29 -0800

    Update CartContext.js

--- context/CartContext.js
+++ context/CartContext.js
@@ -4,36 +4,9 @@ const AppContext = createContext();
 
 export function AppWrapper({ children }) {
   const [products, setProducts] = useState([])
-  let sharedState = {/* whatever you want for example (and use it as state)
-    todos,
-    setTodos,
-    refreshTodos,
-    updateTodos,
-    deleteTodos,
-    addTodods
-    
-  */
+  let sharedState = {
     products,
     setProducts
-  }
-
-
-  // const refreshProducts = async () => { EXAMPLE
-  //   try {
-  //     const res = await fetch('/fetchUrl')
-  //     const latestTodos = await res.json()
-  //     setProducts(latestTodos)
-  //   } catch (err) {
-  //     console.log(err)
-  //   }
-  // }
-
-  function addProduct(newProduct) {
-
-  }
-
-  async function purchase() {
-
   }
 
   return (
@@ -42,10 +15,6 @@ export function AppWrapper({ children }) {
     </AppContext.Provider>
   );
 }
-
-//if not using useContent
-//export {AppProvider, AppContext} 
-//would then be like const {setTodos} = useContext()
 
 export function useAppContext() {
   return useContext(AppContext);
