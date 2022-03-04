9be89ac35c50e1bd6ec3df9c8b48a1b92dd9dcee
James McArthur <77163783+jamezmca@users.noreply.github.com>
2022-03-04 07:10:33 -0800

    akmost done

--- components/Header.js
+++ components/Header.js
@@ -28,29 +28,31 @@ export default function Header() {
 
     return (
         // <div onClick={checkout}>{products.length}</div>
-        <nav className='flex items-center white shadow sticky top-0 relative z-50 bg-white relative'>
-            {displayCheckout && <div ref={modalRef} className='absolute bg-white shadow border border-gray-200 border-solid z-50 top-0 h-screen w-screen sm:w-72 right-0 flex flex-col gap-2 px-2'>
+        <nav className='flex items-center white shadow-lg sticky top-0 relative z-50 bg-white relative'>
+            {displayCheckout && <div ref={modalRef} className='absolute bg-white shadow border border-gray-200 border-solid z-50 top-0 h-screen w-screen sm:w-80 right-0 flex flex-col gap-2 px-2'>
                 <div className='overflow-auto flex-1'>
-                    <div className=''>
+                    <div className='flex justify-between items-center'>
+                        <h1 className='text-4xl py-4'>CART</h1>
                         <div className='ml-auto w-fit p-2 cursor-pointer select-none transition duration-300 opacity-50' onClick={() => setDisplayCheckout(false)}>╳</div>
                     </div>
+                    <hr className='py-2'/>
                     {Object.keys(state.products).map((productId, index) => {
                         const product = state.prices.find(val => val.id === productId)
                         const size = ''
                         const number = ''
-                        return <div key={index} className="border-b border-solid border-gray-100 flex justify-between items-center text-xs font-extralight px-2 py-2">
-                            <div>
+                        return <div key={index} className="border-l border-solid border-gray-100 text-xs font-extralight p-2">
+                            <div className='flex items-center justify-between'>
                                 <p className='truncate'>{product.product.name}</p>
-                                <p></p>
+                                <p>${product.unit_amount / 100}</p>
                             </div>
-                            <p>${product.unit_amount / 100}</p>
+                            <p></p>
                         </div>
                     })}
                 </div>
                 <button className=' m-1 shadow bg-black text-white font-light text-sm py-2 transition duration-300 hover:opacity-50 select-none'>CHECKOUT</button>
             </div>}
             <h1 onClick={() => Router.push('/')}
-                className={'px-4 py-6 pl-14 font-normal select-none flex-1 text-center sm:text-4xl cursor-pointer transition hover:opacity-80 ' + ` ${styles.title}`}>
+                className={'px-4 py-6 sm:py-14 pl-14 font-normal select-none flex-1 text-center sm:text-4xl cursor-pointer transition hover:opacity-80 ' + ` ${styles.title}`}>
                 MOONGLADE
             </h1>
             <div className='relative cursor-pointer grid place-items-center' onClick={() => setDisplayCheckout(!displayCheckout)}>
--- components/PurchaseCard.js
+++ components/PurchaseCard.js
@@ -6,7 +6,7 @@ export default function PurchaseCard(props) {
 
     return (
         <div onClick={() => Router.push(`/${price.id}`)}
-            className='w-60 h-80 shadow-lg border border-solid border-gray-100 cursor-pointer transition hover:opacity-50 bg-white'>
+            className='w-60 h-80 shadow-md border border-solid border-gray-100 cursor-pointer transition hover:opacity-50 bg-white'>
             {price.product.images && (
                 <div className='h-60 object-contain'>
                     <img src={price.product.images[0]} alt={price.product.name} />
